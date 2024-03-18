ab68ce289fda8e63af7c54f6ba01e33d16731357
Mohamed Shamil PV <112370619+ShamilShamsudheen@users.noreply.github.com>
2024-03-18 06:11:29 +0530

    Task form updated to task component

--- src/app/module/tasks/tasks.component.ts
+++ src/app/module/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ApiService } from '../../../Services/apiService/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
@@ -10,42 +11,57 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent implements OnInit{
-primaryColour: string|undefined = '#1976d2';
-secondaryColour: string|undefined;
-loadingTemplate!: TemplateRef<Element>;
-onClickView(arg0: any) {
-throw new Error('Method not implemented.');
-}
-
-OnClickHome() {
-  this.router.navigate(['/groups'])
-}
-  public loading:boolean = false;
-  taskData:any;
-  private groupId!:number;
-  private projectId!:number;
+export class TasksComponent implements OnInit {
+  taskForm!:FormGroup;
+  showTaskForm: boolean = false;
+  onCancel() {
+    this.showTaskForm = false;
+  }
+  onSubmit() {
+    this.taskForm.valid ? console.log(this.taskForm.value):console.log("not valid");
+    ;
+    
+  }
+  primaryColour: string | undefined = '#1976d2';
+  secondaryColour: string | undefined;
+  loadingTemplate!: TemplateRef<Element>;
+  onClickTaskCreate() {
+    this.showTaskForm = true;
+  }
+  public loading: boolean = false;
+  taskData: any;
+  private groupId!: number;
+  private projectId!: number;
   constructor(
-    private apiService:ApiService,
-    private route:ActivatedRoute,
-    private router:Router,
-    private toastr:ToastrService
-    ){}
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private fb: FormBuilder
+  ) {
+    this.taskForm = this.fb.group({
+      description:['',Validators.required],
+      dueDate:['',Validators.required],
+      assigneeMail:['',Validators.required],
+      // createdBy:['',Validators.required],
+      currentStatus:['',Validators.required],
+    })
+  }
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.groupId = parseInt(params['groupId']);
       this.projectId = parseInt(params['projectId']);
     })
-    console.log(this.projectId,this.groupId,"groupId and projectId");
-    this.loading= true;
-    this.apiService.tasks(this.groupId,this.projectId)
-    .subscribe(
-      (res)=>{
-       
-        this.taskData = res;
-        this.loading = false;
-      }
-    )
+    console.log(this.projectId, this.groupId, "groupId and projectId");
+    this.loading = true;
+    this.apiService.tasks(this.groupId, this.projectId)
+      .subscribe(
+        (res) => {
+
+          this.taskData = res;
+          this.loading = false;
+        }
+      )
   }
 
 }
